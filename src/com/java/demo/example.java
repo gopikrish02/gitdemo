eae548158925bb61c781da12643a763d0fd5e2c9
Gopi Krishna <gopikrish02@gmail.com>
2017-06-26 01:25:32 +0800

    conflict1

--- src/com/java/demo/example.java
+++ src/com/java/demo/example.java
@@ -1,5 +1,7 @@
 public class example{
  public static void main(String args[]){
+  System.out.println("My first program from dev branch. conflict");
   System.out.println("My first program from dev branch. creatting confilct");
+  System.out.println("My first program from dev branch. creatting confilct");  
  }
 }
